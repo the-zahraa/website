c0aad1b74a647b73d02608ef8424f0384a0e7cc6
electron <eleproneuqua@gmail.com>
2025-01-24 22:29:41 +0330

    Updated project with new files and features

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css";
+
+
+const Header=() =>{
+    return{
+
+        <header className = "header">
+            <nav className="nav">
+                <ul className="nav-links">
+                    <li>Services</li>
+                    <li>Projects</li>
+                    <li>About Me</li>
+                    <li>Blog</li>
+                </ul>
+
+            </nav>
+        </header>
+
+        
+    }
+}
+
+export default Header;
